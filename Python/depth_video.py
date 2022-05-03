5b10e5284f8124147a71d1d4b148728d257f01d7
Girish Krishnan <gikrishnan@ucsd.edu>
2022-05-02 18:02:57 -0700

    recorded video in pyrealsense

---
+++ Python/depth_video.py
@@ -0,0 +1,41 @@
+import pyrealsense2 as rs
+
+num_frames = 100
+
+pipeline = rs.pipeline()
+config = rs.config()
+
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_record_to_file('./test.bag')
+
+profile = pipeline.start(config)
+
+for _ in range(num_frames):
+    frames = pipeline.wait_for_frames()
+    depth = frames.get_depth_frame()
+    color = frames.get_color_frame()
+    if not depth or not color: continue
+
+    
+
+pipeline.stop()
