02ab1849ba4dfa5ba2d3880f4caa9b817931b6d9
Girish-Krishnan <gikrishnan@ucsd.edu>
2024-01-14 17:24:44 -0800

    testing calibration part

--- Reconstruction/Camera.py
+++ Reconstruction/Camera.py
@@ -10,9 +10,9 @@ try:
     from tqdm import tqdm
     import os
     import time
+    import pyrealsense2 as rs
     from PIL import Image, ImageFilter
     import matplotlib.pyplot as plt
-    import pyrealsense2 as rs
 except ImportError as e:
     print("Warning: Unable to import one or more modules due to the following error: ", e)
     pass
